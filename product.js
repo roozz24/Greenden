1e46d46eebb6d9127f70adddb7d2c4e600926a85
roozz24 <fedelhenry.7@gmail.com>
2025-08-30 12:01:33 +0530

    updated search

--- product.js
+++ product.js
@@ -4,6 +4,7 @@ const productGrid = document.getElementById('productGrid');
 searchInput.addEventListener('input', function() {
     const searchTerm = searchInput.value.toLowerCase();
     const products = productGrid.querySelectorAll('.productCard');
+    const productNotFound = document.getElementById('productNotFound');
 
     Array.from(products).forEach(function(product) {
         const productName = product.getElementsByTagName('h1')[0].textContent.toLowerCase();
@@ -11,8 +12,10 @@ searchInput.addEventListener('input', function() {
 
         if (productName.includes(searchTerm) || productDescription.includes(searchTerm)) {
             product.style.display = 'block';
+            productNotFound.classList.add('hidden');
         } else {
             product.style.display = 'none';
+            productNotFound.classList.remove('hidden');
         }
     });
 });
