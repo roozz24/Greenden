3befd916d4d5b61c118dcbf2a4986aa7e5cf80a6
roozz24 <fedelhenry.7@gmail.com>
2025-08-30 12:18:28 +0530

    updated search

--- product.js
+++ product.js
@@ -1,21 +1,27 @@
 const searchInput = document.getElementById('Search');
 const productGrid = document.getElementById('productGrid');
 
-searchInput.addEventListener('input', function() {
+searchInput.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
     const products = productGrid.querySelectorAll('.productCard');
     const productNotFound = document.getElementById('productNotFound');
+    let found = false;
 
-    Array.from(products).forEach(function(product) {
+    Array.from(products).forEach(function (product) {
         const productName = product.getElementsByTagName('h1')[0].textContent.toLowerCase();
         const productDescription = product.getElementsByTagName('p')[0].textContent.toLowerCase();
 
         if (productName.includes(searchTerm) || productDescription.includes(searchTerm)) {
             product.style.display = 'block';
-            productNotFound.classList.add('hidden');
+            found = true;
         } else {
             product.style.display = 'none';
+        }
+    });
+
+        if (found) {
+            productNotFound.classList.add('hidden');
+        } else {
             productNotFound.classList.remove('hidden');
         }
     });
-});
